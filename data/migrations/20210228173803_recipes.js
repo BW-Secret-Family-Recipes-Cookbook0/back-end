a72d39a66e58c5fcd3c02c5d9f87b0b290df64e9
Michael Sanchez <michaelsanchez42@live.com>
2021-02-28 21:17:56 -0700

    added schema

--- data/migrations/20210228173803_recipes.js
+++ data/migrations/20210228173803_recipes.js
@@ -1,8 +1,28 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("users", (tbl) => {
+      tbl.increments();
+      tbl.string("usernamae", 128).unique().notNullable();
+      tbl.string("password", 128).notNullable();
+    })
+    .createTable("recipes", (tbl) => {
+      tbl.increments("id");
+      tbl.string("title", 255).notNullable();
+      tbl.string("source").notNullable();
+      tbl.string("ingredients").notNullable();
+      tbl.string("instructions").notNullable();
+      tbl.string("category").notNullable();
+      tbl.string("private").notNullable().defaultTo("true");
+      tbl
+        .integer("user_id")
+        .notNullable()
+        .unsigned()
+        .references("users.id")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("recipes").dropTableIfExists("users");
 };
